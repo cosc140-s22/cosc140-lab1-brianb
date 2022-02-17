1661350cc125e55d2d73e9f6bce3b5a7cd0a5f16
Brian Buys <99026557+bbuys520@users.noreply.github.com>
2022-02-17 20:15:41 +0000

    Should be working perfectly now

--- problem1.py
+++ problem1.py
@@ -1,6 +1,22 @@
 #
-# Name:
-# Collaborator(s):
-#
+# Name: Brian
+# Collaborator(s): None :(   
+# 
+
+seconds = input("How many seconds? ")
+seconds = int(seconds)
+secondsS = seconds%(3600*24)%60
+
+minutes = seconds%(3600*24)//60%60
+
+hours = seconds%86400//3600
+
+days = seconds//86400
+
+print("minutes:",minutes)
+print("days",days)
+print("hours",hours)
+print("seconds",secondsS)
+
 
 
